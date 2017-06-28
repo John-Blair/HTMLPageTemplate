6fa5705127cbb64305ab93d73fba0419205f6925
John <john_blair@hotmail.com>
2017-06-28 17:04:51 +0100

    Basic html page layout

---
+++ HTMLPageTemplate/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="document_ready.js" />
+/// <reference path="jquery.plugin.js" />
+/// <reference path="jquery-3.1.1.js" />
+/// <reference path="jquery-3.1.1.slim.js" />
+/// <reference path="popupprovider.js" />
+/// <reference path="revealingmodule.js" />
